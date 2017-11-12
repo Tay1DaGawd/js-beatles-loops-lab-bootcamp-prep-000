fb10ffc93f84726d1517fe38b52ca4cd8ec9e7af
Tashan O'Neal <tdoneal91@gmail.com>
2017-11-12 01:09:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, length) {
   for (let i = 0; i < musicians.length; i++) {
-    theBeatlesPlay.push(`${musicians [i]} plays ${instruments [i]}`)
+    array.push(`${musicians [i]} plays ${instruments [i]}`)
   }
   return theBeatlesPlay
 }
