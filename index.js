5c8418205880d11b06992c8cc03e828077adafcc
Tashan O'Neal <tdoneal91@gmail.com>
2017-11-12 00:54:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function BeatlesLoops() {
 var theBeatlesPlay = []
-  for (let i = 0, i<musicians.length, i++) {
+  for (let i = 0, i < musicians.length, i++) {
     theBeatlesPlay.push(musicians, instruments)
   }
   return theBeatlesPlay
